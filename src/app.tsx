2bf38afdc2818035c5ad06709d35cf5947777786
Nilay Savant <nilayofficial@gmail.com>
2021-04-26 20:20:29 +0530

    Minor fixes

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import Tauri from 'tauri/api'
 import './app.css'
 
 export function App(): React.ReactElement {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [rustMsg, setRustMsg] = useState('')
 
   return (
@@ -11,19 +12,20 @@ export function App(): React.ReactElement {
       <h1>Hello World from Tauri Typescript React!</h1>
       <button
         onClick={() => {
-          // @ts-expect-error TODO
-          Tauri.promisified({
-            cmd: 'performRequest',
-            endpoint: 'dummy endpoint arg',
-            body: ['This', 'is', 'a', 'message', 'from', 'the', 'web'],
-          })
-            // @ts-expect-error TODO
-            .then(Tauri.registerResponse)
-            .then((res) => {
-              setRustMsg(res.message)
-            })
-            // @ts-expect-error TODO
-            .catch(Tauri.registerResponse)
+          // // @ts-expect-error TODO
+          // Tauri.promisified({
+          //   cmd: 'performRequest',
+          //   endpoint: 'dummy endpoint arg',
+          //   body: ['This', 'is', 'a', 'message', 'from', 'the', 'web'],
+          // })
+          //   // @ts-expect-error TODO
+          //   .then(Tauri.registerResponse)
+          //   .then((res) => {
+          //     setRustMsg(res.message)
+          //   })
+          //   // @ts-expect-error TODO
+          //   .catch(Tauri.registerResponse)
+          console.log(Tauri)
         }}
       >
         Get a Message from Rust
